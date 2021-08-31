0b99de690aecb392c4f455a28a2e664dbf46e866
redieight <redietalx@gmail.com>
2021-08-31 15:43:54 +0300

    hell

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints holberton
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints the alphabet in lower case
+ * Can only use putchar
+ *
+ * Return: 0 for success
+*/
+int main(void)
+{
+printf("Holberton\n");
+return(0);
+}
+
