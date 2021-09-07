bfe4690db97ede5a23d22497bd22f8f28900fe03
redieight <redietalx@gmail.com>
2021-09-07 11:29:50 +0300

    final work

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,46 @@
-#include "main.h""
+#include "main.h"
 
 /**
- * _atoi - converts string to int
- * @s: string to convert
- * Description: converts string to int considering all negatives
- * Return: integer
- **/
-
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
 int _atoi(char *s)
 {
-	int i, val, sign;
+	int i, d, n, len, f, digit;
 
-	val = 0;
-	sign = 1;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	for (i = 0; s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'); i++)
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			sign = sign * -1;
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
 
-	for (i = 0; s[i] != 0; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-			val = val * 10 + sign * (s[i] - '0');
-		if (val != 0 && !(s[i] >= '0' && s[i] <= '9'))
-			return (val);
-	}
-	return (val);
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
