bc0e880105e24240d7979c1b4b8298d6273331ba
redieight <redietalx@gmail.com>
2021-10-15 05:03:16 +0300

    adf

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,23 @@
 #include "main.h"
-
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- *
- * Return: the converted number
+ * binary_to_uint - converts binary to unsigned int
+ * @b: binary string
+ * Return: decimal # or 0 if fails
  */
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int dec_val = 0;
+	unsigned int i = 0, decimal = 0;
 
-if (!b)
-return (0);
-
-
-for (i = 0; b[i]; i++)
-{
-if (b[i] < '0' || b[i] > '1')
-return (0);
-dec_val = 2 * dec_val + (b[i] - '0');
-}
- return (dec_val);
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		decimal <<= 1;
+		if (b[i] == '1')
+			decimal ^= 1;
+		i++;
+	}
+	return (decimal);
 }
